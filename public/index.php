7ab4fd144d0741e18c7433a7c26d6f2925bd6626
Hang Zhao <hang_zhao@gsd.harvard.edu>
2026-03-23 22:55:18 -0400

    add favicon

--- public/index.php
+++ public/index.php
@@ -19,6 +19,7 @@ $bodyClass = 'bg-' . $bgStyle;
   <meta charset="utf-8">
   <title><?= htmlspecialchars($config['site_name']) ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/svg+xml" href="/uploads/logo.svg">
   <link rel="stylesheet" href="css/test.css">
 </head>
 
